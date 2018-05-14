7d3449c1a172807bd649ece1524c72dc8dcace32
NICO-LAPTOP\Nico <nico.mazek@googlemail.com>
2018-05-14 11:12:24 +0200

    Touch controller test

---
+++ Assets/Scripts/TouchController.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TouchController : MonoBehaviour {
+
+    public OVRInput.Controller controller;
+
+    private Rigidbody _rigidbody;
+
+    public void Awake() {
+        _rigidbody = GetComponent<Rigidbody>();
+    }
+
+    public void Update() {
+        transform.localPosition = OVRInput.GetLocalControllerPosition(controller);
+        transform.localRotation = OVRInput.GetLocalControllerRotation(controller);
+
+        if(OVRInput.Get(OVRInput.Axis1D.PrimaryIndexTrigger) != 0 || OVRInput.Get(OVRInput.Axis1D.SecondaryHandTrigger) != 0) {
+            _rigidbody.AddForce(Camera.main.transform.forward);
+        }
+    }
+}
