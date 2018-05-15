e7fde2a41ba85c66e1d7e1b365c90da3df68039f
NICO-LAPTOP\Nico <nico.mazek@googlemail.com>
2018-05-15 11:51:04 +0200

    Removed experimental movement, added different teleportation

--- Assets/Scripts/TeleporterScript.cs
+++ Assets/Scripts/TeleporterScript.cs
@@ -4,66 +4,52 @@ using UnityEngine;
 
 public class TeleporterScript : MonoBehaviour {
 
-    //formulas taken from: https://www.youtube.com/watch?v=iLlWirdxass
-
-    public float velocity;
-    public float angle;
-    public int resolution = 10;
-
     public Transform leftHandAnchor;
 
-    private bool _pressedTrigger;
-
+    private Rigidbody _rigidbody;
     private LineRenderer _lineRenderer;
 
-    private Vector3[] _linePoints;
-    private float g; //force of gravity on the y axis
-    private float radianAngle;
+    private Ray _ray;
+    private Vector3 _teleportPoint;
 
     public void Awake() {
-        _pressedTrigger = false;
+        _rigidbody = GetComponent<Rigidbody>();
+
+        _rigidbody.isKinematic = true;
+        _rigidbody.freezeRotation = true;
 
         _lineRenderer = leftHandAnchor.GetComponent<LineRenderer>();
     }
 
     public void Update() {
-        //if left index trigger is pressed down
-        if (OVRInput.Get(OVRInput.Axis1D.PrimaryIndexTrigger) != 0) {
-            _linePoints = new Vector3[resolution + 1];
+        //if either A or X is held down
+        if(OVRInput.Get(OVRInput.Button.One) || OVRInput.Get(OVRInput.Button.Three)) {
+            if (!_lineRenderer.enabled) _lineRenderer.enabled = true;
 
-            radianAngle = Mathf.Deg2Rad * angle;
-            float maxDistance = (velocity * velocity * Mathf.Sin(2 * radianAngle)) / g;
+            _ray.origin = leftHandAnchor.transform.position;
+            _ray.direction = leftHandAnchor.transform.forward;
 
-            //fill array
-            for(int i = 0; i <= resolution; i++) {
-                float t = (float)i / (float)resolution;
-                _linePoints[i] = CalculateArcPoint(t, maxDistance);
+            RaycastHit hit;
+
+            if(Physics.Raycast(_ray, out hit)) {
+                Vector3[] linePoints = new Vector3[2];
+                linePoints[0] = _ray.origin;
+                linePoints[1] = hit.point;
+
+                _lineRenderer.SetPositions(linePoints);
+                Debug.Log("Draw lines");
+
+                _teleportPoint = hit.point;
             }
-
-            //draw line
-            //_lineRenderer.SetVertexCount(resolution + 1); //might not be needed
-            _lineRenderer.SetPositions(_linePoints);
-
-            _pressedTrigger = true;
         }
 
-        //if the left index trigger was previously pressed and now is released
-        if(OVRInput.Get(OVRInput.Axis1D.PrimaryIndexTrigger) == 0 && _pressedTrigger) {
-            transform.Translate(_linePoints[_linePoints.Length - 1]); //teleport to last vector in the array
+        //if either A or X is released
+        if(OVRInput.GetUp(OVRInput.Button.One) || OVRInput.GetUp(OVRInput.Button.Three)) {
+            _lineRenderer.enabled = false;
 
-            _linePoints = new Vector3[0];
-            //_lineRenderer.SetVertexCount(0); //might not be needed
-            _lineRenderer.SetPositions(_linePoints); //apply an empty array to not draw anything
+            transform.position = _teleportPoint;
 
-            _pressedTrigger = false;
-
-            Debug.Log("Left index trigger released");
+            Debug.Log("Teleporting");
         }
-    }
-
-    private Vector3 CalculateArcPoint(float t, float maxDistance) {
-        float x = t * maxDistance;
-        float y = x * Mathf.Tan(radianAngle) - ((g * x * x) / (2 * velocity * velocity * Mathf.Cos(radianAngle) * Mathf.Cos(radianAngle)));
-        return new Vector3(x, y);
     }
 }
