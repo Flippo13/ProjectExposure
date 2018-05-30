6689ca3e6acc0b95ab813e313366869bef4c4b3b
NICO-LAPTOP\Nico <nico.mazek@googlemail.com>
2018-05-30 11:28:53 +0200

    Grab test setup, added fading effect to teleportation, minor player prefab adjustments (added post processing behaviour)

---
+++ Assets/Scripts/Player/TeleportFade.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TeleportFade : OVRScreenFade {
+
+    private bool _secondFade;
+
+    public void StartTeleportFade() {
+        _secondFade = false;
+        StartCoroutine(FadeTeleport(0, 1)); //first fade out
+    }
+
+    IEnumerator FadeTeleport(float startAlpha, float endAlpha) {
+        float elapsedTime = 0.0f;
+        while (elapsedTime < fadeTime) {
+            elapsedTime += Time.deltaTime;
+            currentAlpha = Mathf.Lerp(startAlpha, endAlpha, Mathf.Clamp01(elapsedTime / fadeTime));
+            SetMaterialAlpha();
+
+            if(elapsedTime >= fadeTime && !_secondFade) {
+                Debug.Log("Start fade out");
+                _secondFade = true;
+                StartCoroutine(FadeTeleport(1, 0)); //fade in when done with fading out
+            }
+            yield return new WaitForEndOfFrame();
+        }
+    }
+}
