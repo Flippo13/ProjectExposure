8be296943b55d930857de739980c1c8334e2619f
felix denatris <felixdenatris@hotmail.nl>
2018-06-01 11:45:26 +0200

    Fish Flocking

---
+++ Assets/Scripts/FlockingFish/FlockingFish.cs
@@ -0,0 +1,74 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FlockingFish : MonoBehaviour {
+
+    public LayerMask fishLayer; 
+    public float cohesionRange;
+    public float seperationRange; 
+    public float maxSpeed;
+    public float rotationSpeed; 
+
+    private Vector3 _calculatedCohesion;
+    private Vector3 _calculatedSeperation;
+    private float speed;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        transform.Translate(new Vector3(0, 0, speed * Time.deltaTime));
+
+        if (Random.Range(0, 5) < 1)
+        {
+            _calculatedCohesion = CalculateCohesion();
+            _calculatedSeperation = CalculateSeperation();
+
+            Vector3 direction = (_calculatedCohesion + _calculatedSeperation)- transform.position; 
+            if (direction != Vector3.zero)
+            {
+                transform.rotation = Quaternion.Slerp(transform.rotation, Quaternion.LookRotation(direction), rotationSpeed * Time.deltaTime);
+
+            }   
+        }
+	}
+
+    private Vector3 CalculateCohesion()
+    {
+        Vector3 cohesion = Vector3.zero;
+        int neighbourCount = 0;
+
+        //array full off fish that are in range
+        Collider[] neighbours = Physics.OverlapSphere(transform.position, cohesionRange, fishLayer);
+
+        foreach (Collider neighbour in neighbours)
+        {
+            cohesion += neighbour.transform.position;
+            neighbourCount++; 
+        }
+
+        cohesion /= neighbourCount; 
+
+        return cohesion; 
+    }
+
+    private Vector3 CalculateSeperation()
+    {
+        Vector3 seperation = Vector3.zero;
+        float neighbourCount = 0;
+
+        //array full off fish that are in range
+        Collider[] neighbours = Physics.OverlapSphere(transform.position, seperationRange, fishLayer);
+
+        foreach (Collider neighbour in neighbours)
+        {
+            seperation -= this.transform.position - neighbour.transform.position;
+            neighbourCount++;
+        }
+
+        return seperation; 
+    }
+}
--- Assets/Scripts/FlockingFish/FlockingGlobal.cs
+++ Assets/Scripts/FlockingFish/FlockingGlobal.cs
@@ -4,13 +4,44 @@ using UnityEngine;
 
 public class FlockingGlobal : MonoBehaviour {
 
+    public GameObject goalIndicator;
+    public GameObject fish;
+    public int fishCount; 
+
+    private BoxCollider _fishTank;
+    private Vector3 _goal;
+    private int[] _fishies;
+
 	// Use this for initialization
 	void Start () {
-		
+        _fishTank = GetComponent<BoxCollider>();
+        _fishies = new int[fishCount];
+
+        for (int i = 0; i < _fishies.Length; i++)
+        {
+            Instantiate(fish, new Vector3(Random.Range(_fishTank.bounds.min.x, _fishTank.bounds.max.x), Random.Range(_fishTank.bounds.min.y, _fishTank.bounds.max.y), Random.Range(_fishTank.bounds.min.z, _fishTank.bounds.max.z)), Quaternion.Euler(new Vector3(-90,0,0))); 
+        }
+
+        _goal = new Vector3(Random.Range(_fishTank.bounds.min.x, _fishTank.bounds.max.x), Random.Range(_fishTank.bounds.min.y, _fishTank.bounds.max.y), Random.Range(_fishTank.bounds.min.z, _fishTank.bounds.max.z));
+        StartCoroutine("FindNewGoalPosWithDelay", 10.0f);
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		
 	}
+
+    IEnumerator FindNewGoalPosWithDelay(float delay)
+    {
+        while(true)
+        {
+            yield return new WaitForSeconds(delay);
+            NewGoalPos(); 
+        }
+    }
+
+    private void NewGoalPos()
+    {
+        _goal = new Vector3(Random.Range(_fishTank.bounds.min.x, _fishTank.bounds.max.x), Random.Range(_fishTank.bounds.min.y, _fishTank.bounds.max.y), Random.Range(_fishTank.bounds.min.z, _fishTank.bounds.max.z));
+    }
+
 }
