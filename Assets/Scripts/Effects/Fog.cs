9bb6a86ddb919c4c7b3afab9bb01c1c02ad7d82f
Daan <daankemps@hotmail.com>
2018-06-02 11:23:14 +0200

    Added gradient fog and made changes to Fog.cs, currently fog setup works - but transparent geometry such as the kelp isn't taking in account with the Depth Buffer. Probably need to ask Mark/Hans/Anyone.

--- Assets/Scripts/Effects/Fog.cs
+++ Assets/Scripts/Effects/Fog.cs
@@ -1,20 +1,57 @@
 ﻿using UnityEngine;
-using System.Collections;
-
-[ExecuteInEditMode]
 public class Fog : MonoBehaviour
 {
+    [SerializeField]
+    private Gradient _fogGradient;
+    [SerializeField]
+    private Material _fogMaterial;
 
-    public Material fogMaterial;
-    public Camera main;
+    [SerializeField]
+    [Range(0, 2)]
+    private float _FogIntensity;
 
-    void Start()
+    [SerializeField]
+    [Range(0, 2)]
+    private float _FogAmount;
+
+    [SerializeField]
+    [Range(32, 256)]
+    private int _textureSize = 128;
+
+    void Awake()
     {
-        main.depthTextureMode = DepthTextureMode.Depth;
+        GenerateTexture();
+        GetComponent<Camera>().depthTextureMode = DepthTextureMode.Depth;
     }
+
+    [ContextMenu("Update")]
+    private void GenerateTexture()
+    {
+        //Create new texture with
+        Texture2D texture = new Texture2D(_textureSize, 1);
+
+        //Some texture settings to make it ideal for the shader
+        texture.alphaIsTransparency = true;
+        texture.anisoLevel = 16;
+        texture.wrapMode = TextureWrapMode.Clamp;
+        texture.filterMode = FilterMode.Trilinear;
+
+        //Add gradient colors to the texture
+        for (int i = 0; i < _textureSize; i++)
+        {
+            texture.SetPixel(i, 1, _fogGradient.Evaluate((float)i / _textureSize));
+        }
+        texture.Apply();
+
+        //Apply generated texture and paremeters to shader
+        _fogMaterial.SetTexture("_ColorRamp", texture);
+        _fogMaterial.SetFloat("_FogIntensity", _FogIntensity);
+        _fogMaterial.SetFloat("_FogAmount", _FogAmount);
+    }
+
 
     void OnRenderImage(RenderTexture source, RenderTexture destination)
     {
-        Graphics.Blit(source, destination, fogMaterial);
+        Graphics.Blit(source, destination, _fogMaterial);
     }
 }
