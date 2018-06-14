da6e13cc255f02ac9afd1d6324e87f9523c81d13
felix denatris <felixdenatris@hotmail.nl>
2018-06-14 10:49:54 +0200

    Slightly optimised fish flocking

--- Assets/Scripts/FlockingFish/FlockingFish.cs
+++ Assets/Scripts/FlockingFish/FlockingFish.cs
@@ -10,49 +10,94 @@ public class FlockingFish : MonoBehaviour {
     public float maxSpeed;
     public float rotationSpeed;
 
-    private new Transform transform; 
+    private FlockingGlobal _fishTank; 
+    private new Transform transform;
+    private SphereCollider _col; 
+    private List<Vector3> _listOfObjectsInRange = new List<Vector3>(); 
 
     private Vector3 _calculatedCohesion;
     private Vector3 _calculatedSeperation;
     private float speed;
-    private Vector3 _goalPos; 
+    private Vector3 _goalPos;
+    private Vector3 direction;
+    private bool _goBack;
 
-	// Use this for initialization
-	void Start () {
+    // Use this for initialization
+    void Start () {
         speed = Random.Range(0.1f, maxSpeed);
-        transform = GetComponent<Transform>(); 
+        transform = GetComponent<Transform>();
+        _col = GetComponent<SphereCollider>();
+        _fishTank = GetComponentInParent<FlockingGlobal>(); 
+        StartCoroutine("CalculateDirectionWithDelay", 0.2f);
+       // _col.radius = cohesionRange; 
 	}
 	
 	// Update is called once per frame
 	void Update () {
+
         transform.Translate(new Vector3(0, 0, speed * Time.deltaTime));
 
-        if (Random.Range(0, 3) < 1)
+        if (direction != Vector3.zero)
         {
-            _calculatedCohesion = CalculateCohesion() + (FlockingGlobal.goal - transform.position);
-            _calculatedSeperation = CalculateSeperation();
-            Debug.Log("cohesion: " + _calculatedCohesion);
-            Debug.Log("seperation: " + _calculatedSeperation);
-
-            Vector3 direction = (_calculatedCohesion + _calculatedSeperation) - transform.position;
-            Debug.Log("direction: " + direction);
-            if (direction != Vector3.zero)
-            {
-                transform.rotation = Quaternion.Slerp(transform.rotation, Quaternion.LookRotation(direction), rotationSpeed * Time.deltaTime);
-            }   
+            transform.rotation = Quaternion.Slerp(transform.rotation, Quaternion.LookRotation(direction), rotationSpeed * Time.deltaTime);
         }
-	}
+    }
 
 
-    private Vector3 CalculateCohesion()
+    private IEnumerator CalculateDirectionWithDelay(float delay)
+    {
+        while (true)
+        {
+            yield return new WaitForSeconds(delay);
+            CalculateDirection(); 
+        }
+    }
+
+    private void CalculateDirection()
+    {
+        if (Vector3.Distance(transform.position, _fishTank.fishTank.bounds.center) >= _fishTank.fishTank.size.x
+           || Vector3.Distance(transform.position, _fishTank.fishTank.bounds.center) >= _fishTank.fishTank.size.y
+           || Vector3.Distance(transform.position, _fishTank.fishTank.bounds.center) >= _fishTank.fishTank.size.z)
+        {
+            _goBack = true;
+        }
+        else
+        {
+            _goBack = false;
+
+        }
+
+        if (_goBack)
+        {
+            direction = _fishTank.fishTank.bounds.center - transform.position;
+            transform.rotation = Quaternion.Slerp(transform.rotation, Quaternion.LookRotation(direction), rotationSpeed * Time.deltaTime);
+            speed = Random.Range(0.6f, maxSpeed);
+            Debug.Log("Going back");
+        }
+
+        else if (Random.Range(0.0f, 10.0f) < 1 && !_goBack)
+        {
+            Collider[] neighbours = Physics.OverlapSphere(transform.position, cohesionRange, fishLayer);
+            Debug.Log("Going to goal");
+            _calculatedCohesion = CalculateCohesion(neighbours) + (_fishTank.Goal - transform.position);
+            _calculatedSeperation = CalculateSeperation(neighbours);
+            //Debug.Log("cohesion: " + _calculatedCohesion);
+            //Debug.Log("seperation: " + _calculatedSeperation);
+
+            direction = (_calculatedCohesion + _calculatedSeperation) - transform.position;
+            //Debug.Log("direction: " + direction);
+        }
+
+    }
+
+    private Vector3 CalculateCohesion(Collider[] neighbours)
     {
         Vector3 cohesion = Vector3.zero;
         int neighbourCount = 0;
         float gSpeed = 0;
 
         //array full off fish that are in range
-        Collider[] neighbours = Physics.OverlapSphere(transform.position, cohesionRange, fishLayer);
-        Debug.Log("neighbours: " + neighbours.Length);
+        //Collider[] neighbours = Physics.OverlapSphere(transform.position, cohesionRange, fishLayer);
         foreach (Collider neighbour in neighbours)
         {
             cohesion += neighbour.transform.position;
@@ -65,19 +110,43 @@ public class FlockingFish : MonoBehaviour {
         return cohesion; 
     }
 
+    private Vector3 CalculateCohesionV2()
+    {
+        Vector3 cohesion = Vector3.zero;
+        int neighbourCount = 0;
+        //float gSpeed = 0;
 
-    private Vector3 CalculateSeperation()
+        
+        foreach (Vector3 neighbourPos in _listOfObjectsInRange)
+        {
+            if (Vector3.Distance(neighbourPos,transform.position) < cohesionRange)
+            {
+                cohesion += neighbourPos;
+                neighbourCount++;
+               // gSpeed += neighbourPos.gameObject.GetComponent<FlockingFish>().speed;
+            }
+        }
+
+        cohesion /= neighbourCount;
+        //speed = gSpeed / neighbourCount;
+        return cohesion;
+    }
+
+    private Vector3 CalculateSeperation(Collider[] neighbours)
     {
         Vector3 seperation = Vector3.zero;
         float neighbourCount = 0;
 
         //array full off fish that are in range
-        Collider[] neighbours = Physics.OverlapSphere(transform.position, seperationRange, fishLayer);
+       // Collider[] neighbours = Physics.OverlapSphere(transform.position, seperationRange, fishLayer);
 
         foreach (Collider neighbour in neighbours)
         {
-            seperation -= this.transform.position - neighbour.transform.position;
-            neighbourCount++;
+            if (Vector3.Distance(neighbour.transform.position, transform.position) < seperationRange)
+            {
+                seperation -= this.transform.position - neighbour.transform.position;
+                neighbourCount++;
+            }
         }
 
         seperation /= neighbourCount;
@@ -85,6 +154,23 @@ public class FlockingFish : MonoBehaviour {
         return seperation; 
     }
 
+
+    private void OnTriggerEnter(Collider other)
+    {
+        Debug.Log("Trigger Entered "); 
+        if (other.tag == "Fish")
+        {
+            _listOfObjectsInRange.Add(other.transform.position);
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.tag == "Fish")
+        {
+            _listOfObjectsInRange.Remove(other.transform.position);
+        }
+    }
 
     private void OnDrawGizmos()
     {
--- Assets/Scripts/FlockingFish/FlockingGlobal.cs
+++ Assets/Scripts/FlockingFish/FlockingGlobal.cs
@@ -8,21 +8,21 @@ public class FlockingGlobal : MonoBehaviour {
     public GameObject fish;
     public int fishCount; 
 
-    private BoxCollider _fishTank;
-    public static Vector3 goal;
+    public BoxCollider fishTank;
+    public Vector3 goal;
     private int[] _fishies;
 
 	// Use this for initialization
 	void Start () {
-        _fishTank = GetComponent<BoxCollider>();
+        fishTank = GetComponent<BoxCollider>();
         _fishies = new int[fishCount];
 
         for (int i = 0; i < _fishies.Length; i++)
         {
-            Instantiate(fish, new Vector3(Random.Range(_fishTank.bounds.min.x, _fishTank.bounds.max.x), Random.Range(_fishTank.bounds.min.y, _fishTank.bounds.max.y), Random.Range(_fishTank.bounds.min.z, _fishTank.bounds.max.z)), Quaternion.Euler(new Vector3(-90,0,0))); 
+            Instantiate(fish, new Vector3(Random.Range(fishTank.bounds.min.x, fishTank.bounds.max.x), Random.Range(fishTank.bounds.min.y, fishTank.bounds.max.y), Random.Range(fishTank.bounds.min.z, fishTank.bounds.max.z)), Quaternion.Euler(new Vector3(-90,0,0)),this.transform); 
         }
 
-        goal = new Vector3(Random.Range(_fishTank.bounds.min.x, _fishTank.bounds.max.x), Random.Range(_fishTank.bounds.min.y, _fishTank.bounds.max.y), Random.Range(_fishTank.bounds.min.z, _fishTank.bounds.max.z));
+        goal = new Vector3(Random.Range(fishTank.bounds.min.x, fishTank.bounds.max.x), Random.Range(fishTank.bounds.min.y, fishTank.bounds.max.y), Random.Range(fishTank.bounds.min.z, fishTank.bounds.max.z));
         StartCoroutine("FindNewGoalPosWithDelay", 10.0f);
 	}
 	
@@ -43,7 +43,7 @@ public class FlockingGlobal : MonoBehaviour {
 
     private void NewGoalPos()
     {
-        goal = new Vector3(Random.Range(_fishTank.bounds.min.x, _fishTank.bounds.max.x), Random.Range(_fishTank.bounds.min.y, _fishTank.bounds.max.y), Random.Range(_fishTank.bounds.min.z, _fishTank.bounds.max.z));
+        goal = new Vector3(Random.Range(fishTank.bounds.min.x, fishTank.bounds.max.x), Random.Range(fishTank.bounds.min.y, fishTank.bounds.max.y), Random.Range(fishTank.bounds.min.z, fishTank.bounds.max.z));
     }
 
 
