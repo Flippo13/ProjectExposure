a80651c0faf3177677f06505ffc34ee82e2709a8
felix denatris <felixdenatris@hotmail.nl>
2018-06-15 00:25:06 +0200

    more cleaning

--- Assets/Scripts/Turbine/Turbine.cs
+++ Assets/Scripts/Turbine/Turbine.cs
@@ -4,24 +4,13 @@ using UnityEngine;
 
 public class Turbine : MonoBehaviour {
 
-    public Transform turbineArea;
     private Animator _anim; 
-    public float dropTime;
-    public float setHeight; 
-
-    private BoxCollider _col;
-    private Vector3 landPos;
 
     // Use this for initialization
     void Start () {
         _anim = GetComponent<Animator>();
 	}
 	
-	// Update is called once per frame
-	void Update () {
-		
-	}
-
     public void Activate()
     {
         //Debug.Log("I still need to have an animation and sound that shows that I work!");
