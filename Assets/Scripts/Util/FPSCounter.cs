22c3e276fa579ad6e6aab2f66cfc4190b57c52d3
NICO-LAPTOP\Nico <nico.mazek@googlemail.com>
2018-06-15 11:38:16 +0200

    Added fps counter, made collider for vacuuming bigger

---
+++ Assets/Scripts/Util/FPSCounter.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class FPSCounter : MonoBehaviour {
+    public Text fpsCount;
+
+    private float _updateTimer;
+    private float _updateInterval;
+
+    public void Awake() {
+        _updateInterval = 1f / 3f;
+    }
+
+    public void Update() {
+        if(_updateTimer >= _updateInterval) {
+            fpsCount.text = "FPS: " + Mathf.Round(1f / Time.deltaTime);
+            _updateTimer = 0f;
+        }
+
+        _updateTimer += Time.deltaTime;
+    }
+}
