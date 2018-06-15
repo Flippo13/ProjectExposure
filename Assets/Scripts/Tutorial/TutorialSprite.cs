f1d6f7cab73b9feb90be8565f4b7deb60fa37138
Daan <daankemps@hotmail.com>
2018-06-15 17:53:46 +0200

    Fixed linerenderer, tutorial buttons should work now.

--- Assets/Scripts/Tutorial/TutorialSprite.cs
+++ Assets/Scripts/Tutorial/TutorialSprite.cs
@@ -8,15 +8,15 @@ public class TutorialSprite : MonoBehaviour
 
     private LineRenderer _lineRenderer;
     private SpriteRenderer _spriteRenderer;
+
     private void Awake()
     {
         _spriteRenderer = GetComponent<SpriteRenderer>();
-        _lineRenderer.SetPosition(0, new Vector3(_spriteRenderer.bounds.min.x, _spriteRenderer.bounds.min.y, _spriteRenderer.bounds.min.z));
-        _lineRenderer.SetPosition(1, transform.parent.position);
+        _lineRenderer = GetComponent<LineRenderer>();
     }
     void Update()
     {
-        transform.LookAt(transform.position + Camera.main.transform.rotation * Vector3.forward, Camera.main.transform.rotation * Vector3.up);
+        transform.rotation = Camera.main.transform.rotation;
         _lineRenderer.SetPosition(0, new Vector3(_spriteRenderer.bounds.min.x, _spriteRenderer.bounds.min.y, _spriteRenderer.bounds.min.z));
         _lineRenderer.SetPosition(1, transform.parent.position);
     }
