8a3696cb739fe37606e12d2fa11db9f88c4783b2
Daan <daankemps@hotmail.com>
2018-06-19 12:01:14 +0200

    Animated (PingPong) Tutorial buttons

--- Assets/Scripts/Tutorial/TutorialSprite.cs
+++ Assets/Scripts/Tutorial/TutorialSprite.cs
@@ -6,6 +6,12 @@ using UnityEngine;
 [RequireComponent(typeof(LineRenderer))]
 public class TutorialSprite : MonoBehaviour
 {
+    public bool useParent;
+    public GameObject _Button;
+    private Color _baseColor;
+    private Color _highlightedColor;
+
+
     [SerializeField]
     private List<Sprite> _sprites = new List<Sprite>();
 
@@ -17,23 +23,35 @@ public class TutorialSprite : MonoBehaviour
 
     private GameObject _parent;
 
-    //float _pingPongFloat;
-    //private Color _buttonColor;
-    //private Material _buttonMaterial;
-  
+    float _pingPongFloat;
+    private Color _buttonColor;
+    private Material _buttonMaterial;
+
     private void Awake()
     {
+        _baseColor = new Color(0.86f, 0.57f, 0, 1);
+        _highlightedColor = new Color(0.86f, 0.57f, 0, 0.8f);
         _spriteRenderer = GetComponent<SpriteRenderer>();
         _lineRenderer = GetComponent<LineRenderer>();
         _parent = transform.parent.gameObject;
 
         _lineRenderer.SetPosition(0, _parent.transform.position);
         _lineRenderer.SetPosition(1, _parent.transform.parent.position);
-        //_buttonMaterial = transform.parent.GetComponent<Renderer>().material;
+        _buttonMaterial = _Button.GetComponent<Renderer>().material;
     }
     void Update()
     {
-        transform.LookAt(transform.position + Camera.main.transform.rotation * Vector3.forward, Camera.main.transform.rotation * Vector3.up);
+        if (useParent)
+        {
+            Vector3 deltaVec = Camera.main.transform.position - transform.parent.position;
+            transform.parent.rotation = Quaternion.LookRotation(deltaVec);
+        }
+        else
+        {
+            Vector3 deltaVec = Camera.main.transform.position - transform.position;
+            transform.rotation = Quaternion.LookRotation(deltaVec);
+        }
+
         _lineRenderer.SetPosition(0, _parent.transform.position);
         _lineRenderer.SetPosition(1, _parent.transform.parent.position);
 
@@ -41,11 +59,11 @@ public class TutorialSprite : MonoBehaviour
         _spriteRenderer.sprite = _sprites[(int)(Time.time * _spriteSpeed) % _sprites.Count];
     }
 
-    //private void LateUpdate()
-    //{
-    //    // Ping pong the button so it pops out
-    //    _pingPongFloat = Mathf.PingPong(Time.time, 1);
-    //    _buttonColor = Color.Lerp(Color.white, Color.blue, _pingPongFloat);
-    //    _buttonMaterial.color = _buttonColor;
-    //}
+    private void LateUpdate()
+    {
+        // Ping pong the button so it pops out
+        _pingPongFloat = Mathf.PingPong(Time.time * 3, 1);
+        _buttonColor = Color.Lerp(_baseColor, _highlightedColor, _pingPongFloat);
+        _buttonMaterial.color = _buttonColor;
+    }
 }
