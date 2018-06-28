37c70a0c2221dc2120ec19e9d957dc6ef34e6e58
NICO-LAPTOP\Nico <nico.mazek@googlemail.com>
2018-06-28 12:16:27 +0200

    Presentation version with fixes (i hope)

--- Assets/Scripts/Companion/AI/CompanionObjective.cs
+++ Assets/Scripts/Companion/AI/CompanionObjective.cs
@@ -55,6 +55,7 @@ public class CompanionObjective : MonoBehaviour {
             else pointerInstance.transform.position = transform.position;
 
             _objectivePointer = pointerInstance.GetComponent<ObjectivePointer>();
+            _objectivePointer.Disable();
         }
     }
 
