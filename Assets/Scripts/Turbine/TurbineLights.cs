4ebd66f4b1dcd04962611f26d9e8eba035a96ae3
felix denatris <felixdenatris@hotmail.nl>
2018-07-03 20:22:59 +0200

    moar

--- Assets/Scripts/Turbine/TurbineLights.cs
+++ Assets/Scripts/Turbine/TurbineLights.cs
@@ -5,13 +5,14 @@ using UnityEngine;
 public class TurbineLights : MonoBehaviour {
 
 
-    public Color turnedOnColor; 
+    public Color turnedOnColor;
+
+    public Color landingColor; 
 
     public float lightIntensityWhenOn; 
 
     private Light _light; 
-
-
+    
 	// Use this for initialization
 	void Start () {
         _light = GetComponent<Light>(); 
@@ -23,4 +24,9 @@ public class TurbineLights : MonoBehaviour {
         _light.color = turnedOnColor;
     }
 
+    public void TurbineIsDropping()
+    {
+        _light.intensity = lightIntensityWhenOn;
+        _light.color = turnedOnColor;
+    }
 }
