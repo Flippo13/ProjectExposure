f176482c040eba6936f5603e26352d06ffd81720
Daan <daankemps@hotmail.com>
2018-07-03 17:29:08 +0200

    Added title

---
+++ Assets/Scripts/Level/Title.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Title : MonoBehaviour
+{
+    public bool _lookAt;
+
+    private void Update()
+    {
+        if (_lookAt)
+        {
+            Vector3 deltaVec = Camera.main.transform.position - transform.position;
+            transform.rotation = Quaternion.LookRotation(-deltaVec);
+        }
+    }
+}
---
+++ Assets/Scripts/Level/TitleDissolve.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TitleDissolve : MonoBehaviour {
+
+
+    [SerializeField]
+    private GameObject _title;
+
+    [SerializeField]
+    private GameObject _titleParticles;
+
+
+    private void OnTriggerExit(Collider other)
+    {
+        if(other.tag == "Player")
+        {
+            _title.GetComponent<Animator>().Play("Titledissolve");
+            _titleParticles.GetComponent<Animator>().Play("Titledissolveparticles");
+            Debug.Log("Player exited");
+        }
+
+       
+    }
+}
