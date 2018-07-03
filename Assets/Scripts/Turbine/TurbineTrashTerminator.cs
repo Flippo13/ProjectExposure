f55706e5992f252fbaaf6fae8536777092a313d0
felix denatris <felixdenatris@hotmail.nl>
2018-07-03 21:31:09 +0200

    THE TRASH TERMINATOR!

---
+++ Assets/Scripts/Turbine/TurbineTrashTerminator.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TurbineTrashTerminator : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+
+
+    private void OnCollisonEnter(Collider other)
+    {
+        if (other.gameObject.layer == Layers.Suckable)
+        {
+            Destroy(other.gameObject); 
+        }
+    }
+}
